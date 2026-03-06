add77035c865136893c72f90708ba850d969c2c8
Soumyasree Mitra <soumyasreemitra2005@gmail.com>
2026-03-06 23:51:47 +0530

    Initialize thread pool scheduler project structure

---
+++ include/threadpool.h
@@ -0,0 +1,35 @@
+#ifndef THREADPOOL_H
+#define THREADPOOL_H
+
+#include <vector>
+#include <thread>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+#include <functional>
+
+class ThreadPool {
+
+public:
+
+    ThreadPool(size_t threads);
+
+    void enqueue(std::function<void()> task);
+
+    ~ThreadPool();
+
+private:
+
+    std::vector<std::thread> workers;
+
+    std::queue<std::function<void()>> tasks;
+
+    std::mutex queue_mutex;
+
+    std::condition_variable condition;
+
+    bool stop;
+
+};
+
+#endif
